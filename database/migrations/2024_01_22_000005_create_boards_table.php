23506cb39abbbaac7dfc64b6aaa3b6a663643734
giyong <akima9a@gmail.com>
2024-02-02 13:20:43 +0900

    add board status

--- database/migrations/2024_01_22_000005_create_boards_table.php
+++ database/migrations/2024_01_22_000005_create_boards_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('content');
+            $table->enum('status', ['running', 'done'])->default('running');
             $table->foreignId('user_id')->constrained('users');
             $table->foreignId('gym_id')->constrained('gyms');
             $table->timestamps();
