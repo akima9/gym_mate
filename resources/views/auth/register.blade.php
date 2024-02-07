8ddfed51fba14b75bb487081f980fd05979becb1
giyong <akima9a@gmail.com>
2024-02-07 16:19:28 +0900

    modify register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
+        <!-- Ninkname -->
         <div>
             <x-input-label for="nickname" :value="__('Nickname')" />
             <x-text-input id="nickname" class="block mt-1 w-full" type="text" name="nickname" :value="old('nickname')" required autofocus autocomplete="nickname" />
@@ -39,6 +39,7 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- Gender -->
         <div class="mt-4">
             <label class="block font-medium text-sm text-gray-700">{{__('Gender')}}</label>
             <div class="flex justify-around bg-gray-50 rounded-md shadow-sm mt-1 py-2">
@@ -53,6 +54,7 @@
             </div>
         </div>
 
+        <!-- Age group -->
         <div class="mt-4">
             <label class="block font-medium text-sm text-gray-700">{{__('Age group')}}</label>
             <select name="age" id="age" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full mt-1">
@@ -66,6 +68,21 @@
                 <option value="80">Eighties</option>
                 <option value="90">Nineties</option>
             </select>
+        </div>
+
+        <!-- Gym list -->
+        <div class="mt-4">
+            <label class="block font-medium text-sm text-gray-700">{{__('Gym')}}</label>
+            <div class="flex mt-1">
+                <div class="flex-1">
+                    <x-text-input onkeydown="chat.handleKeyPress(event)" id="title" name="title" type="text" class="block w-full" required autofocus autocomplete="title" />
+                    <x-input-error class="mt-2" :messages="$errors->get('title')" />
+                </div>
+                <button onclick="" class="inline-flex items-center ml-1 px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">{{ __('Search') }}</button>
+            </div>
+            {{-- <select name="gym_id" id="gym_id" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full mt-1">
+                <option value="1">gym1</option>
+            </select> --}}
         </div>
 
         <div class="flex items-center justify-end mt-4">
