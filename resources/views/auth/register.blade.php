27e3280c81f14f345a8de1c9a45334b37a21be9c
devGiYong <akima9a@gmail.com>
2024-02-10 16:52:07 +0900

    modify register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -52,6 +52,7 @@
                     <label for="woman">Woman</label>
                 </div>
             </div>
+            <x-input-error :messages="$errors->get('gender')" class="mt-2" />
         </div>
 
         <!-- Age group -->
@@ -68,21 +69,7 @@
                 <option value="80">Eighties</option>
                 <option value="90">Nineties</option>
             </select>
-        </div>
-
-        <!-- Gym list -->
-        <div class="mt-4">
-            <label class="block font-medium text-sm text-gray-700">{{__('Gym')}}</label>
-            <div class="flex mt-1">
-                <div class="flex-1">
-                    <x-text-input onkeydown="chat.handleKeyPress(event)" id="title" name="title" type="text" class="block w-full" required autofocus autocomplete="title" />
-                    <x-input-error class="mt-2" :messages="$errors->get('title')" />
-                </div>
-                <button onclick="" class="inline-flex items-center ml-1 px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">{{ __('Search') }}</button>
-            </div>
-            {{-- <select name="gym_id" id="gym_id" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full mt-1">
-                <option value="1">gym1</option>
-            </select> --}}
+            <x-input-error :messages="$errors->get('age')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
