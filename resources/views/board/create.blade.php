8a9e132567233ba857dee06fa832861ca9a37357
giyong <akima9a@gmail.com>
2024-02-22 16:21:05 +0900

    modify create board form

--- resources/views/board/create.blade.php
+++ resources/views/board/create.blade.php
@@ -17,17 +17,28 @@
                             <x-input-error class="mt-2" :messages="$errors->get('title')" />
                         </div>
                         <div class="mb-4">
-                            <x-input-label for="" :value="__('운동 일자')" />
-                            <input type="date" name="" id="" class="mt-1 block border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                            <x-input-label for="trainingDate" :value="__('운동 일자')" />
+                            <input type="date" name="trainingDate" id="trainingDate" class="mt-1 block border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
                         </div>
                         <div class="mb-4">
-                            <x-input-label for="" :value="__('운동 시간')" />
-                            <input type="time" name="" id="" class="mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                            <x-input-label for="trainingTime" :value="__('운동 시간')" />
+                            <input type="time" name="trainingStartTime" id="trainingStartTime" class="mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
                             ~
-                            <input type="time" name="" id="" class="mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                            <input type="time" name="trainingEndTime" id="trainingEndTime" class="mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
                         </div>
                         <div class="mb-4">
-                            <x-input-label for="" :value="__('운동 부위')" />
+                            <x-input-label for="trainingPart" :value="__('운동 부위')" />
+                            <select name="trainingPart" id="trainingPart" onchange="createBoard.handleChange()" class="mt-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+                                <option value="skip">선택해주세요</option>
+                                <option value="chest">가슴</option>
+                                <option value="back">등</option>
+                                <option value="shoulder">어깨</option>
+                                <option value="lowerBody">하체</option>
+                                <option value="biceps">이두</option>
+                                <option value="triceps">삼두</option>
+                                <option value="abs">복부</option>
+                            </select>
+                            <div class="trainingParts"></div>
                         </div>
                         <div class="mb-4">
                             <x-input-label for="content" :value="__('추가 내용')" />
@@ -40,4 +51,48 @@
             </div>
         </div>
     </div>
+
+    @push('scripts')
+        <script>
+            const createBoard = {
+                handleChange: () => {
+                    let selectedOption = document.querySelector('#trainingPart').selectedOptions[0];
+                    let selectedValue = selectedOption.value;
+                    let selectedText = selectedOption.text;
+                    let hiddenInput = document.querySelector('.' + selectedValue); //null이 아니면 중복선택임.
+
+                    if (selectedValue !== 'skip' && hiddenInput === null) {
+                        createBoard.makeHiddenInput(selectedValue, selectedText);
+                        createBoard.makeBadge(selectedValue, selectedText);
+                    }
+                },
+                makeHiddenInput: (selectedValue, selectedText) => {
+                    let trainingPartsDiv = document.querySelector('.trainingParts');
+                    let hiddenInput = document.createElement('input');
+                    hiddenInput.type = 'hidden';
+                    hiddenInput.value = selectedValue;
+                    hiddenInput.className = selectedValue;
+                    trainingPartsDiv.appendChild(hiddenInput);
+                },
+                makeBadge: (selectedValue, selectedText) => {
+                    let trainingPartsDiv = document.querySelector('.trainingParts');
+                    let badge = document.createElement('span');
+                    badge.textContent = selectedText;
+                    badge.className = "inline-flex items-center rounded-md bg-blue-50 px-2 py-1 font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10 mt-1 mr-1";
+                    
+                    let deleteLink = document.createElement('a');
+                    deleteLink.textContent = 'X';
+                    deleteLink.className = 'ml-1 hover:cursor-pointer';
+                    deleteLink.onclick = () => {
+                        trainingPartsDiv.removeChild(badge);
+                        let hiddenInput = document.querySelector('.' + selectedValue);
+                        trainingPartsDiv.removeChild(hiddenInput);
+                    };
+                    
+                    badge.appendChild(deleteLink);
+                    trainingPartsDiv.appendChild(badge);
+                }
+            }
+        </script>
+    @endpush
 </x-app-layout>
