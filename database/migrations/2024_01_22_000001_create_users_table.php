024bce32b0d10917b1cb40fd96c461ee70feae7d
giyong <akima9a@gmail.com>
2024-03-27 14:21:12 +0900

    add TO-DO

--- database/migrations/2024_01_22_000001_create_users_table.php
+++ database/migrations/2024_01_22_000001_create_users_table.php
@@ -11,6 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
+        /**
+         * TO-DO
+         * 1. 마지막 로그인 일자 추가
+         * 2. 친절도 추가(같이 운동하는게 확정되고 운동 종료 시간 이후에 친절도 평가가 있어야함)
+         * 2-1. 친절도는 2.5kg씩 증가 또는 감소 되도록 하자.
+         */
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nickname');
