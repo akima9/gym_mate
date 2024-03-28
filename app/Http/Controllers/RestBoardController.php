22ee72f573f74ea68c1486b023a944693464c9c5
giyong <akima9a@gmail.com>
2024-03-28 16:47:15 +0900

    add RestBoardController method

--- app/Http/Controllers/RestBoardController.php
+++ app/Http/Controllers/RestBoardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Dtos\BoardDto;
+use App\Http\Requests\StoreBoardRequest;
+use App\Http\Requests\UpdateBoardRequest;
+use App\Models\Board;
 use App\Services\BoardService;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -34,40 +38,57 @@ class RestBoardController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreBoardRequest $request)
     {
-        //
+        $request->validated();
+        $boardDto = new BoardDto($request);
+        $board = $this->boardService->save($boardDto);
+        return $board;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Board $board)
     {
-        //
+        $board->trainingParts = json_decode($board->trainingParts);
+        return $board;
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Board $board)
     {
-        //
+        $this->authorize('update', $board);
+        $board->trainingParts = json_decode($board->trainingParts);
+        return $board;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateBoardRequest $request, Board $board)
     {
-        //
+        /**
+         * TO-DO
+         * update 실패 경우 추가해야함.
+         */
+        $this->authorize('update', $board);
+        $request->validated();
+        
+        $boardDto = new BoardDto($request);
+        // $updatedCount = $this->boardService->update($board, $boardDto);
+        $updatedBoard = $this->boardService->findById($board->id);
+        return $updatedBoard;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Board $board)
     {
-        //
+        $this->authorize('delete', $board);
+        $this->boardService->delete($board->id);
     }
 }
