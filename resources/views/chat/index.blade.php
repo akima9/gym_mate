d8a401a9e6a44a8010b4c3a1838983d2a9bf477b
giyong <akima9a@gmail.com>
2024-04-12 12:41:21 +0900

    modify index chat

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -10,8 +10,14 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     @if ($chatPartners->isEmpty())
-                        <p>채팅 기록이 없습니다.</p>
-                        <p>같은 GYM을 이용하고 동일한 성별을 가진 사용자의 게시물을 통해 1:1 채팅이 가능합니다.</p>
+                        <p class="font-semibold mb-2">채팅 기록이 없습니다.</p>
+                        <p class="mb-2">같은 GYM을 이용하고, 동일한 성별을 가진 사용자의 게시물을 통해 1:1 채팅이 가능합니다.</p>
+                        <div>
+                            <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">동일 GYM</span> + 
+                            <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">동일 성별</span> +
+                            <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">게시글 채팅버튼</span> =
+                            <span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">채팅 가능</span>
+                        </div>
                     @endif
                     @foreach ($chatPartners as $chatPartner)
                         <a onclick="goToChat({{$chatPartner->id}})" class="hover:cursor-pointer m-1">
